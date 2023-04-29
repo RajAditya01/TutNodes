4757b1d87e56f32fe33a47e609911a03dbb53998
Aditya <adityavishal903@gmail.com>
2023-04-30 00:04:47 +0530

    1

---
+++ Variable_And_DataType/declareVariable.js
@@ -0,0 +1,17 @@
+//Variables
+
+// var name='Aditya';
+// console.log(name);
+
+let price=250;
+const name='Aditya Raj';
+console.log(price,name);
+
+let isMarried= false;
+console.log(typeof isMarried);
+
+let colors=undefined;
+console.log(typeof colors);
+
+let selectColor="red";
+console.log(typeof selectColor);
