7289cb0032afb568b17f83421095381cd743332f
Aditya <adityavishal903@gmail.com>
2023-04-30 00:28:02 +0530

    2

--- Variable_And_DataType/declareVariable.js
+++ Variable_And_DataType/declareVariable.js
@@ -3,15 +3,27 @@
 // var name='Aditya';
 // console.log(name);
 
-let price=250;
-const name='Aditya Raj';
-console.log(price,name);
+// let price=250;
+// const name='Aditya Raj';
+// console.log(price,name);
 
-let isMarried= false;
-console.log(typeof isMarried);
+// let isMarried= false;
+// console.log(typeof isMarried);
 
-let colors=undefined;
-console.log(typeof colors);
+// let colors=undefined;
+// console.log(typeof colors);
 
-let selectColor="red";
-console.log(typeof selectColor);
+// let selectColor="red";
+// console.log(typeof selectColor);
+
+
+let name='Aditya Raj';
+let age=18;
+let hobby='reading techinical news';
+
+let user{
+    name:'Aditya';
+    age: 19;
+    hobbyy :'rading'
+};
+console.log(typeof user);
