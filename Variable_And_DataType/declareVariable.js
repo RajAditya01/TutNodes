8d69828a59d6e68f05a2b9479cd4886936895781
Aditya <adityavishal903@gmail.com>
2023-05-01 00:33:10 +0530

    2

--- Variable_And_DataType/declareVariable.js
+++ Variable_And_DataType/declareVariable.js
@@ -1,4 +1,4 @@
-//Variables
+//#Variables-->
 
 // var name='Aditya';
 // console.log(name);
@@ -17,13 +17,59 @@
 // console.log(typeof selectColor);
 
 
-let name='Aditya Raj';
-let age=18;
-let hobby='reading techinical news';
+// let name='Aditya Raj';
+// let age=18;
+// let hobby='reading techinical news';
 
-let user{
-    name:'Aditya';
-    age: 19;
-    hobbyy :'rading'
-};
-console.log(typeof user);
+// let user{
+//     name:'Aditya';
+//     age: 19;
+//     hobbyy :'rading'
+// };
+// console.log(typeof user);
+
+//#Arrays-->
+
+// let friend =['Aditya','Gautam','punnit', 'Akshat']
+// console.log(friend);
+// console.log(typeof friend);
+// console.log(friend.length);
+
+// #Function-->
+// function showMyName(){
+//     console.log('My Name is Aditya');
+//     console.log("I Am Aditya Raj");
+// }
+// showMyName();
+
+function showMyName(name) {
+    //console.log('My name is '+name);
+}
+showMyName('aditya');
+showMyName('raj');
+showMyName('vishal');
+showMyName('snehit');
+
+// let num1 =30;
+// let num2=50;
+// let sum=num1+num2;
+// console.log(sum)
+
+// function calculateSum(num1,num2){
+//     const sum= num1+num2;
+//     return sum;
+// }
+
+// const result= calculateSum(30,20);
+// console.log(result);
+
+// Template Literals-->
+let name = 'Aditya';
+//console.log('My name is '+name);
+// console.log(`my name is ${name}`);
+
+// console.log(`$`{30+20});
+
+//new line-->
+let message = `my name is \n aditya`;
+console.log(message);
