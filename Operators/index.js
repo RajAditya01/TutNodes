faf4424b7655ea0f317a46106cbd82d87ffa3462
Aditya <adityavishal903@gmail.com>
2023-06-09 18:42:29 +0530

    3

--- Operators/index.js
+++ Operators/index.js
@@ -1,24 +1,35 @@
-//Arthmetic Operators
+// Arthmetic Operators
 
 // let num1= 10;
 // let nmm2= 5;
-
 // console.log(num1+num2);
 // console.log(num1-num2);
 // console.log(num1*num2);
 // console.log(num1/num2);
 
-//Assignment Operators
-let num1=10;
-let num2=num1;
-console.log(num1,num2);
+//  Assignment Operators
 
-let num3=num1+5;
-num3 +=5;
-console.log(num3);
+// let num1=10;
+// let num2=num1;
+// console.log(num1,num2);
 
-let num4=num1-5;
-num4 -=5;
+// let num3=num1+5;
+// num3 +=5;
+// console.log(num3);
 
-let num6 = num1*6;
-num6 *=6;
+// let num4=num1-5;
+// num4 -=5;
+
+// let num6 = num1*6;
+// num6 *=6;
+
+//comparision operators
+// > < >= <= === !==
+
+let price=500;
+console.log(price > 300);
+console.log(price >=500);
+console.log(price < 300);
+console.log(price <=500);
+console.log(price === 300);
+console.log(price !==500);
