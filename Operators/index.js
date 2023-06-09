73f250713f1424165b78d2ce50f6ef2122ca04ac
Aditya <adityavishal903@gmail.com>
2023-06-09 18:29:37 +0530

    .

--- Operators/index.js
+++ Operators/index.js
@@ -1,9 +1,14 @@
 //Arthmetic Operators
 
-let num1= 10;
-let nmm2= 5;
+// let num1= 10;
+// let nmm2= 5;
 
-console.log(num1+num2);
-console.log(num1-num2);
-console.log(num1*num2);
-console.log(num1/num2);
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1/num2);
+
+//Assignment Operators
+let num1=10;
+let num2=num1;
+console.log(num1,num2);
