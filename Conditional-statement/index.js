a3a0360a6eadc37fe711f691551927fd3ce57143
Aditya <adityavishal903@gmail.com>
2023-06-09 23:29:06 +0530

    1

--- Conditional-statement/index.js
+++ Conditional-statement/index.js
@@ -10,11 +10,30 @@
 
 // }
 
-let num=10;
-if(num>0){
-    console.log('this is +ve nos');
-}else if(num<0){
-    console.log("this is -ve nos");
-}else{
-    console.log("this is 0");
+// let num=10;
+// if(num>0){
+//     console.log('this is +ve nos');
+// }else if(num<0){
+//     console.log("this is -ve nos");
+// }else{
+//     console.log("this is 0");
+// }
+
+
+//switch case
+let col='white';
+
+switch(color){
+    case 'black;
+    console.log('this is black');
+    break;
+
+    case 'white';
+    console.log('this is white');
+    break;
+
+    case 'red';
+    console.log('this is red');
+    break;
+
 }
