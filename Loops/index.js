324e5772375511647d75bd41ec9f33be7b62b015
Aditya <adityavishal903@gmail.com>
2023-06-10 11:23:26 +0530

    04

--- Loops/index.js
+++ Loops/index.js
@@ -7,19 +7,37 @@
 //     console.log('Hi Aditya...!',i);
 // }
 
-//While loop
-let i=100;
-while(i<=5){
-    console.log('Hii Raj...!',i);
-    i++;
+// //While loop
+// let i=100;
+// while(i<=5){
+//     console.log('Hii Raj...!',i);
+//     i++;
+// }
+
+// // Do while loop
+
+// let j=100;
+// do{
+//     console.log('Hii udemy...')
+//     j++;
+// }while(j <= 5)
+
+// console.log(j);
+
+//for in loop
+
+const objs={
+    name : 'Aditya',
+    age : 18,
 }
 
-// Do while loop
+for (let key in objs){
+    console.log(key, objs[key]);
+}
 
-let j=100;
-do{
-    console.log('Hii udemy...')
-    j++;
-}while(j <= 5)
+let nums=[1.2,3,4,5];
+console.log(nums);
 
-console.log(j);
+for (let index in numbers){
+    console.log(index,numbers[index]);
+}
