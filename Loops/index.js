ca6fb83d0dcfb72e56b16d4d4263f470d4eae0bc
Aditya <adityavishal903@gmail.com>
2023-06-10 11:08:48 +0530

    03

--- Loops/index.js
+++ Loops/index.js
@@ -1,8 +1,25 @@
 //for loop
-for(let i=0; i<=5; i++){
-    console.log('Hi Aditya...!' ,i);
+// for(let i=0; i<=5; i++){
+//     console.log('Hi Aditya...!' ,i);
+// }
+
+// for(let i=5; i>=0; i--){
+//     console.log('Hi Aditya...!',i);
+// }
+
+//While loop
+let i=100;
+while(i<=5){
+    console.log('Hii Raj...!',i);
+    i++;
 }
 
-for(let i=5; i>=0; i--){
-    console.log('Hi Aditya...!',i);
-}
+// Do while loop
+
+let j=100;
+do{
+    console.log('Hii udemy...')
+    j++;
+}while(j <= 5)
+
+console.log(j);
