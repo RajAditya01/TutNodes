943d362e36227fe819edbe1fb1bc1b98dc4d4bd3
Aditya <adityavishal903@gmail.com>
2023-06-10 14:05:51 +0530

    2

--- String/index.js
+++ String/index.js
@@ -1,5 +1,14 @@
-let country="India";
-console.log(country);
-console.log(typeof country);
-console.log(country[7]);
-console.log(country.charAt(7));
+// let country="India";
+// console.log(country);
+// console.log(typeof country);
+// console.log(country[7]);
+// console.log(country.charAt(7));
+
+let msge="hii my name is aditya";
+console.log(msge);
+console.log(msge.charAt(1));
+console.log(msge.toUpperCase());
+console.log(msge.toLowerCase());
+console.log(includes('aditya'));
+console.log(msge.startsWith('hii'));
+console.log(msge.endsWith('aditya'));
