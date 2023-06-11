09c176c09628017d509f6f9faca2c8584c53749b
Aditya <adityavishal903@gmail.com>
2023-06-11 15:15:03 +0530

    5

--- String/index.js
+++ String/index.js
@@ -13,18 +13,32 @@
 // console.log(msge.startsWith('hii'));
 // console.log(msge.endsWith('aditya'));
 
-let msge="hii my name is aditya";
-let tem= 123;
-console.log(tem.toString());
-console.log(typeof tem.toString());
+// let msge="hii my name is aditya";
+// let tem= 123;
+// console.log(tem.toString());
+// console.log(typeof tem.toString());
 
-let m1='HI';
-let m2=' MY NAME';
-let m3=' IS ADITYA';
+// let m1='HI';
+// let m2=' MY NAME';
+// let m3=' IS ADITYA';
 
-let m= m1.concat(m2,m3);
-//Same as--> let m=m1+m2+m3;
-console.log(m);
+// let m= m1.concat(m2,m3);
+// //Same as--> let m=m1+m2+m3;
+// console.log(m);
 
-//Split method
-console.log(msge.split(' '));
+// //Split method
+// console.log(msge.split(' '));
+
+let msge="hi my name is aditya";
+console.log(msge.slice(0,5));
+console.log(msge.slice(6,12));
+console.log(msge.slice(2));
+console.log(msge.slice());
+console.log(msge.slice(0));
+console.log(msge.slice(-12,-1));
+
+
+console.log(msge.substring(0,3));
+
+//only difference blw slice, substring substr is in substr 2nd parameter is length and slice & substring 2nd parameter is a length ;
+console.log(msge.substr(0,4));
