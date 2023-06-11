c5fa278b9faa67cb2106de578b0d6269ab0dfa32
Aditya <adityavishal903@gmail.com>
2023-06-11 14:49:59 +0530

    1

--- String/index.js
+++ String/index.js
@@ -4,11 +4,11 @@
 // console.log(country[7]);
 // console.log(country.charAt(7));
 
-let msge="hii my name is aditya";
-console.log(msge);
-console.log(msge.charAt(1));
-console.log(msge.toUpperCase());
-console.log(msge.toLowerCase());
-console.log(includes('aditya'));
-console.log(msge.startsWith('hii'));
-console.log(msge.endsWith('aditya'));
+// let msge="hii my name is aditya";
+// console.log(msge);
+// console.log(msge.charAt(1));
+// console.log(msge.toUpperCase());
+// console.log(msge.toLowerCase());
+// console.log(includes('aditya'));
+// console.log(msge.startsWith('hii'));
+// console.log(msge.endsWith('aditya'));
