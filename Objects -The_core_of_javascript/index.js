ec09644c2281918d7cef978cdf7f18505adfc014
Aditya <adityavishal903@gmail.com>
2023-06-11 16:11:49 +0530

    03

--- Objects -The_core_of_javascript/index.js
+++ Objects -The_core_of_javascript/index.js
@@ -21,22 +21,39 @@
 
 //Manipulation
 
+// const user = {
+//     name: 'Aditya',
+//     age: 18,
+//     hobby: 'Reading',
+//     isMarried: false,
+//   };
+
+//   //Add
+//   user.Email= 'adityavishal903@gmail.com';
+//   user['Phone Number']= 807648957;
+
+//   //Update
+//   user.isMarried= false;
+//   user.age=user.age+7;
+
+//   //Delete
+//   delete user.isMarried;
+
+//   console.log(user);
+
+
+//Object Method and This keyword
+
 const user = {
     name: 'Aditya',
     age: 18,
     hobby: 'Reading',
     isMarried: false,
+    friend: ['Deepak', 'Akshat', 'Rishav'],
+    calculateAge: function(){
+      console.log(`I am ${this.age} year old`);
+    }
   };
 
-  //Add
-  user.Email= 'adityavishal903@gmail.com';
-  user['Phone Number']= 807648957;
-
-  //Update
-  user.isMarried= false;
-  user.age=user.age+7;
-
-  //Delete
-  delete user.isMarried;
-
+  user.calculateAge();
   console.log(user);
