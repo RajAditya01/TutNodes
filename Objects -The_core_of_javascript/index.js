90d4999c5c7d6de17d23f2e2335ba792dcdf0f83
Aditya <adityavishal903@gmail.com>
2023-06-11 16:19:11 +0530

    4

--- Objects -The_core_of_javascript/index.js
+++ Objects -The_core_of_javascript/index.js
@@ -44,16 +44,36 @@
 
 //Object Method and This keyword
 
-const user = {
-    name: 'Aditya',
-    age: 18,
-    hobby: 'Reading',
-    isMarried: false,
-    friend: ['Deepak', 'Akshat', 'Rishav'],
-    calculateAge: function(){
-      console.log(`I am ${this.age} year old`);
-    }
-  };
+// const user = {
+//     name: 'Aditya',
+//     age: 18,
+//     hobby: 'Reading',
+//     isMarried: false,
+//     friend: ['Deepak', 'Akshat', 'Rishav'],
+//     calculateAge: function(){
+//       console.log(`I am ${this.age} year old`);
+//     }
+//   };
 
-  user.calculateAge();
-  console.log(user);
+//   user.calculateAge();
+//   console.log(user);
+
+
+//Traversing Object
+const user = {
+  name: 'Aditya',
+  age: 18,
+  hobby: 'Reading',
+  isMarried: false,
+  friend: ['Deepak', 'Akshat', 'Rishav'],
+  email: 'abc@gmail.com',
+};
+
+//one way
+for(let key in user){
+  console.log(key, user[key]);
+}
+
+//two way
+console.log(Object.keys(user));
+console.log(Object.values(user));
