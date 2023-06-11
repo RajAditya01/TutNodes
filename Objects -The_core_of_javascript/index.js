0925b0e98354f35ff047cb8dabaa6dea2c7c77c5
Aditya <adityavishal903@gmail.com>
2023-06-11 18:51:57 +0530

    08

--- Objects -The_core_of_javascript/index.js
+++ Objects -The_core_of_javascript/index.js
@@ -185,3 +185,6 @@
 // Math.ceil(x)	Returns x rounded up to its nearest integer
 // Math.floor(x)	Returns x rounded down to its nearest integer
 // Math.trunc(x)	Returns the integer part of x (new in ES6)
+
+
+//JSON Data Format
