6727ae4275f776c2cf22392a90d7280427c9f70f
Aditya <adityavishal903@gmail.com>
2023-06-12 11:11:42 +0530

    2

--- Functions/index.js
+++ Functions/index.js
@@ -1,20 +1,31 @@
 //Function Declaration
 
-function aboutMe(name, age){
-    const message=`I am ${name} and i am ${age} year old`;
-    console.log(message);
-}
+// function aboutMe(name, age){
+//     const message=`I am ${name} and i am ${age} year old`;
+//     console.log(message);
+// }
 
-//function call
-aboutMe('Aditya',20);
-aboutMe('Dhiraj',18);
+// //function call
+// aboutMe('Aditya',20);
+// aboutMe('Dhiraj',18);
 
-//sum function
+// //sum function
 
-function calSum(num1,num2){
-    let sum= num1+num2;
+// function calSum(num1,num2){
+//     let sum= num1+num2;
+//     return sum;
+// }
+
+// let result=calSum(50,20);
+// console.log(result);
+
+
+//lac 2
+// Default Parameter
+
+function calSum(num1,num2=50,num3=60){
+    let sum= num1+num2+num3;
     return sum;
 }
 
-let result=calSum(50,20);
-console.log(result);
+console.log(calSum(10));
