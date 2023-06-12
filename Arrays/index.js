2f9f058603440a9b4beb862eb998bccb696cee77
Aditya <adityavishal903@gmail.com>
2023-06-12 13:14:05 +0530

    2

--- Arrays/index.js
+++ Arrays/index.js
@@ -1,13 +1,21 @@
 //l-1
 let friend =['Aditya','akshat','punit','gautam'];
 
-console.log(friend);
-console.log(friend[0]);
-console.log(friend[2]);
-console.log(friend[3]);
+// console.log(friend);
+// console.log(friend[0]);
+// console.log(friend[2]);
+// console.log(friend[3]);
 
-confirm.log(friend.length);
+// confirm.log(friend.length);
 
 //l-2
 
+const num=[12,13,14,17,18]
 
+num.unshift(9,10,11);
+num.push(19,20,21);
+
+
+num.splice(3,0,15,16);
+
+console.log(num);
