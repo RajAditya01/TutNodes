b815db15dd64d0e93c24b6e2c0d3fe5b0215bf80
Aditya <adityavishal903@gmail.com>
2023-06-12 11:06:49 +0530

    1

--- Functions/index.js
+++ Functions/index.js
@@ -8,3 +8,13 @@ function aboutMe(name, age){
 //function call
 aboutMe('Aditya',20);
 aboutMe('Dhiraj',18);
+
+//sum function
+
+function calSum(num1,num2){
+    let sum= num1+num2;
+    return sum;
+}
+
+let result=calSum(50,20);
+console.log(result);
