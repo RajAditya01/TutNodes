df3ba178974fea225c0dc21f1a83556e9c822836
Aditya <adityavishal903@gmail.com>
2023-06-12 11:02:28 +0530

    1

---
+++ Functions/index.js
@@ -0,0 +1,10 @@
+//Function Declaration
+
+function aboutMe(name, age){
+    const message=`I am ${name} and i am ${age} year old`;
+    console.log(message);
+}
+
+//function call
+aboutMe('Aditya',20);
+aboutMe('Dhiraj',18);
