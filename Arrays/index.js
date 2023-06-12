5909db5817a69482b983deb5099acd1e9dbb1f1c
Aditya <adityavishal903@gmail.com>
2023-06-12 13:21:43 +0530

    3

--- Arrays/index.js
+++ Arrays/index.js
@@ -18,4 +18,15 @@ num.push(19,20,21);
 
 num.splice(3,0,15,16);
 
-console.log(num);
+// console.log(num);
+
+//l -3
+const num1=[1,2,3,4,5,6,7,8];
+
+// num1.shift();
+// num1.shift();
+// num1.pop();
+// num1.pop();
+
+num1.splice(2,2);
+console.log(num1);
