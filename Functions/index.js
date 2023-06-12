8c5807374f7489ca13b7cf879d7a43c5a59bf0cb
Aditya <adityavishal903@gmail.com>
2023-06-12 11:17:59 +0530

    3

--- Functions/index.js
+++ Functions/index.js
@@ -29,3 +29,12 @@ function calSum(num1,num2=50,num3=60){
 }
 
 console.log(calSum(10));
+
+//l-3
+//function expression
+
+const calSumExp= function(num1,num2){
+    let sum=num1+num2;
+    return sum;
+}
+console.log(calSumExp(50,60));
