2e94419e3c6d65d6fa679f0f6f9184978c989c9f
Aditya <adityavishal903@gmail.com>
2023-06-13 13:58:00 +0530

    2

--- Object oriented/index.js
+++ Object oriented/index.js
@@ -1,18 +1,32 @@
 //l-1
 //Factory Function
 
-function user (name, age){
-    const userObj={
-        name,
-        age,
-        walk: function(){
-            console.log(`${name} started walking`);
-        },
+// function user (name, age){
+//     const userObj={
+//         name,
+//         age,
+//         walk: function(){
+//             console.log(`${name} started walking`);
+//         },
+//     };
+//     return userObj;
+// }
+
+// const user1= user('Aditya',20);
+// const user2= user('RAj',18);
+
+// console.log(user1, user2);
+
+//L-2
+function user(name, age){
+    console.log(this);
+    this.name=name;
+    this.age=age;
+    this.walk= function(){
+        console.log('Aditya Starting walking');
     };
-    return userObj;
+    console.log(this);
 }
 
-const user1= user('Aditya',20);
-const user2= user('RAj',18);
-
-console.log(user1, user2);
+const user1 = new user(Aditya,18);
+console.log(user1);
