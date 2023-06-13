59d0d4dfa87922e43563885097103acb1475b21f
Aditya <adityavishal903@gmail.com>
2023-06-13 10:08:11 +0530

    2

--- ES6-ES7-Features/index.js
+++ ES6-ES7-Features/index.js
@@ -1,22 +1,45 @@
 //ES6 Feature : Array and object Destructuring
 
-function greeting(user){
+// function greeting(user){
 
-    const {name,age, address} =user;
-    const{long , lat}= address;
-    console.log(`Hello ${name} and age ${age} from ${lat}, ${long}`);
+//     const {name,age, address} =user;
+//     const{long , lat}= address;
+//     console.log(`Hello ${name} and age ${age} from ${lat}, ${long}`);
+// }
+
+// const user ={
+//     name: 'Aditya',
+//     age: 20,
+//     address:{
+//         long:343.343,
+//         lat:2323.23,
+//     },
+// };
+
+// greeting(user);
+
+// let [a,b,c,d] =[1,2,3,4];
+// console.log(a,b,c,d);
+
+//l -2
+//Data Structure : Set
+let set =new Set();
+set.add(100);
+set.add(200);
+set.add(300);
+set.add(100);
+
+console.log(set);
+console.log(set.size);
+set.delete(200);
+console.log(set);
+console.log(set.size);
+
+for(let i of set){
+    console.log(i);
 }
 
-const user ={
-    name: 'Aditya',
-    age: 20,
-    address:{
-        long:343.343,
-        lat:2323.23,
-    },
-};
+set.forEach((i) => console.log(i));
 
-greeting(user);
-
-let [a,b,c,d] =[1,2,3,4];
-console.log(a,b,c,d);
+set.clear();
+console.log(set);
