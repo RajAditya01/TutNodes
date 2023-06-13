194d28c69566228a0d59209267f3c9fdc49e7eb8
Aditya <adityavishal903@gmail.com>
2023-06-13 12:45:17 +0530

    3

--- Regular Expression/index.js
+++ Regular Expression/index.js
@@ -1,5 +1,14 @@
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions
- 
- const regExp= /aditya/i;
 
- console.log(regExp.test('hi i am Aditya'))
+ const regExp= /aditya/ig;
+
+//  console.log(regExp);
+//  console.log(regExp,source);
+//  console.log(regExp.exec('Hi  Hello Aditya'))
+
+//  console.log(regExp.test('hi i am Aditya '));
+
+ const str ='hi i am Aditya Aditya Aditya';
+ console.log(str.match(regExp));
+ console.log(str.search(regExp));
+ console.log(str.replace(regExp,'Raj'));
