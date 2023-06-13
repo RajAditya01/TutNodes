d64aea783bf26f0495ba478c489baca61ca2b7c8
Aditya <adityavishal903@gmail.com>
2023-06-13 12:04:35 +0530

    1

---
+++ Error Handlings/index.js
@@ -0,0 +1,12 @@
+let pi = 3.141592654;
+
+//error no 1
+//console.log(pi.toFixed(101))
+
+//error no-2
+let country= "india";
+console.log(country.toUpperCase());
+
+let javaIsFun = true;
+//console.log(javaIsFun.toUpperCase());
+console.log('Hi Aditya');
