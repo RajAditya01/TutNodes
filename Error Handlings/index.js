e5a48faa7983b698752d008d00b806e8d013f4d6
Aditya <adityavishal903@gmail.com>
2023-06-13 12:09:52 +0530

    2

--- Error Handlings/index.js
+++ Error Handlings/index.js
@@ -4,9 +4,20 @@ let pi = 3.141592654;
 //console.log(pi.toFixed(101))
 
 //error no-2
-let country= "india";
-console.log(country.toUpperCase());
+// let country= "india";
+// console.log(country.toUpperCase());
 
-let javaIsFun = true;
-//console.log(javaIsFun.toUpperCase());
-console.log('Hi Aditya');
+// let javaIsFun = true;
+// //console.log(javaIsFun.toUpperCase());
+// console.log('Hi Aditya');
+
+//l-2
+
+function div(a,b){
+    if(b===0){
+        throw new SyntaxError("Second Parameter can't be zero");
+    }
+    return a/b;
+}
+
+console.log(div(4,0));
