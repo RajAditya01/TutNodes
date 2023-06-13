9f4739e55076a51c8f568f0edda5ebf89a1cc5a0
Aditya <adityavishal903@gmail.com>
2023-06-13 09:52:08 +0530

    1

---
+++ ES6-ES7-Features/index.js
@@ -0,0 +1,19 @@
+//ES6 Feature : Array and object Destructuring
+
+function greeting(user){
+
+    const {name,age, address} =user;
+    const{long , lat}= address;
+    console.log(`Hello ${name} and age ${age} from ${lat}, ${long}`);
+}
+
+const user ={
+    name: 'Aditya',
+    age: 20,
+    address:{
+        long:343.343,
+        lat:2323.23,
+    },
+};
+
+greeting(user);
