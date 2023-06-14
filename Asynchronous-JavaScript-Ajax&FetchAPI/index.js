1c1028ed72aeb86f378347a857b96259d827e713
Aditya <adityavishal903@gmail.com>
2023-06-14 23:49:57 +0530

    2

--- Asynchronous-JavaScript-Ajax&FetchAPI/index.js
+++ Asynchronous-JavaScript-Ajax&FetchAPI/index.js
@@ -1,18 +1,35 @@
 // Asynchronous Programming
 
-console.log('Before');
+// console.log('Before');
 
-function fetchUser(){
+// function fetchUser(){
+//     setTimeout(function(){
+//         console.log('Hi my name is Aditya');
+//         return {
+//             name: 'Aditya',
+//             age: 20,
+//         };
+//     },2000);
+// }
+
+// const user =fetchUser();
+// console.log(user);
+
+// console.log('After');
+
+//l2
+fetchUser(1234, function(user){
+    console.log(user);
+})
+
+function fetchUser(userId,callback){
     setTimeout(function(){
-        console.log('Hi my name is Aditya');
-        return {
+        console.log('Hi my name is aditya');
+        const fetchedUser ={
+            id: userId,
             name: 'Aditya',
-            age: 20,
+            emai: 'aditya@gmail.com'
         };
+        callback(fetchUser);
     },2000);
 }
-
-const user =fetchUser();
-console.log(user);
-
-console.log('After');
