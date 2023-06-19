49b862082a81f30d976ab969d2a4be1277c6dd63
Aditya <adityavishal903@gmail.com>
2023-06-19 15:19:41 +0530

    4

--- Asynchronous-JavaScript-Ajax&FetchAPI/index.js
+++ Asynchronous-JavaScript-Ajax&FetchAPI/index.js
@@ -37,31 +37,47 @@
 //l-3 
 // callback Hell
 
-fetchUser(1234, function(user){
-    console.log(user);
-    sendMail(user.email,function(response){
-        console.log(response.success);
-    });
+// fetchUser(1234, function(user){
+//     console.log(user);
+//     sendMail(user.email,function(response){
+//         console.log(response.success);
+//     });
+// });
+
+// function sendMail(userEmail,callback){
+
+//     setTimeout(function(){
+//         console.log('message send');
+//         const response ={success:true};
+//         callback(response);
+//     },3000);
+// }
+
+
+// function fetchUser(userId,callback){
+//     setTimeout(function(){
+//         console.log('Hi my name is aditya');
+//         const fetchedUser ={
+//             id: userId,
+//             name: 'Aditya',
+//             emai: 'aditya@gmail.com'
+//         };
+//         callback(fetchUser);
+//     },2000);
+// }
+
+//l-4
+//promise
+
+const promise= new promise(function(resolve,reject){
+    setTimeout(() =>{
+        reject(new Error('Hi there have some error'));
+    },2000);
 });
 
-function sendMail(userEmail,callback){
-
-    setTimeout(function(){
-        console.log('message send');
-        const response ={success:true};
-        callback(response);
-    },3000);
-}
-
-
-function fetchUser(userId,callback){
-    setTimeout(function(){
-        console.log('Hi my name is aditya');
-        const fetchedUser ={
-            id: userId,
-            name: 'Aditya',
-            emai: 'aditya@gmail.com'
-        };
-        callback(fetchUser);
-    },2000);
-}
+console.log(promise);
+promise.then(function(result){
+    console.log(result);
+}).catch(function(err){
+    console.log(err);
+})
